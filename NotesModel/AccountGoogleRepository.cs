1fc74bd441bdd39ede8ad24c6701d7ddf3fe7837
jkenoyer <kenoyer130@gmail.com>
2013-05-05 14:58:45 -0400

    added forms authentication

---
+++ NotesModel/AccountGoogleRepository.cs
@@ -0,0 +1,38 @@
+﻿using notes.Model.Repository;
+using NotesInterfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Text.RegularExpressions;
+
+namespace NotesModel
+{
+    public class AccountGoogleRepository : IAccountRepository
+    {
+        Regex googleID = new Regex("id=(.*?)$");
+
+        public int Login(string id)
+        {
+            string openID = googleID.Match(id).Groups[1].Value;
+
+            int? accountID = 0;
+            
+            using (var conn = new SqlConnection(Connection.Value))
+            {
+                conn.Open();
+                accountID = conn.Query<int>("select accountID from dbo.Account WHERE openID='@openID'", new { openID }).FirstOrDefault();
+
+                if (accountID == null || accountID == 0)
+                {
+                    accountID = conn.Execute("insert into dbo.Account(OpenID) VALUES ('@openID'); SELECT SCOPE_IDENTITY();", new { openID });
+                }
+            }
+
+            return accountID.Value;
+        }
+    }
+}
---
+++ notes/Controllers/LoginController.cs
@@ -0,0 +1,75 @@
+﻿using DotNetOpenAuth.OpenId;
+using DotNetOpenAuth.OpenId.Extensions.SimpleRegistration;
+using DotNetOpenAuth.OpenId.RelyingParty;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Security;
+using DotNetOpenAuth.Messaging;
+using NotesInterfaces.Repository;
+using NotesModel;
+
+namespace notes.Controllers
+{
+    public class LoginController : Controller
+    {
+        private IAccountRepository accountRepository;
+        public LoginController()
+        {
+            accountRepository = new AccountGoogleRepository();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LogOn()
+        {
+            var openid = new OpenIdRelyingParty();
+            IAuthenticationResponse response = openid.GetResponse();
+
+            if (response != null)
+            {
+                switch (response.Status)
+                {
+                    case AuthenticationStatus.Authenticated:
+                        Session["accountID"] = accountRepository.Login(response.ClaimedIdentifier);                        
+                        FormsAuthentication.SetAuthCookie(Session["accountID"].ToString(), true);
+                        return new RedirectResult("/", false);
+                    case AuthenticationStatus.Canceled:
+                        ModelState.AddModelError("loginIdentifier",
+                            "Login was cancelled at the provider");
+                        break;
+                    case AuthenticationStatus.Failed:
+                        ModelState.AddModelError("loginIdentifier",
+                            "Login failed using the provided OpenID identifier");
+                        break;
+                }
+            }
+
+            return View();
+        }
+
+        [System.Web.Mvc.AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult LogOn(string loginIdentifier)
+        {
+            if (!Identifier.IsValid(loginIdentifier))
+            {
+                ModelState.AddModelError("loginIdentifier",
+                            "The specified login identifier is invalid");
+                return View();
+            }
+            else
+            {
+                var openid = new OpenIdRelyingParty();
+                IAuthenticationRequest request = openid.CreateRequest(Identifier.Parse(loginIdentifier));
+                return request.RedirectingResponse.AsActionResult();
+            }
+        }
+    }
+}
